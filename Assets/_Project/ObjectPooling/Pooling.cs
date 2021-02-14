1ad08a93d16cbb440dcf28f9e710052af60daae0
Radosław Rychlik <radusek4@gmail.com>
2021-02-14 10:26:22 +0100

    project init

---
+++ Assets/_Project/ObjectPooling/Pooling.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class Pooling
+{
+    public const int DEFAULT_POOL_SIZE = 3;
+    public static Dictionary<int, Pool> pools = new Dictionary<int, Pool>();
+
+    public class Pool
+    {
+        private readonly GameObject prefab;
+        private readonly Queue<GameObject> inactiveObjects;
+        private int nextId = 1;
+
+        public readonly HashSet<int> memberIDs;
+
+
+        public Pool(GameObject prefab, int initialQuantity)
+        {
+            this.prefab = prefab;
+            inactiveObjects = new Queue<GameObject>(initialQuantity);
+            memberIDs = new HashSet<int>();
+        }
+
+        public GameObject Spawn(Vector3 pos, Quaternion rot, Transform parent = null)
+        {
+            GameObject obj;
+            if (inactiveObjects.Count == 0)
+            {
+                obj = Object.Instantiate(prefab, pos, rot);
+                obj.name = prefab.name + " (" + (nextId++) + ")";
+
+                memberIDs.Add(obj.GetInstanceID());
+            }
+            else
+            {
+                obj = inactiveObjects.Dequeue();
+
+                if (obj == null)
+                {
+                    return Spawn(pos, rot, parent);
+                }
+            }
+
+            obj.transform.SetParent(parent, false);
+            obj.transform.SetPositionAndRotation(pos, rot);
+            obj.SetActive(true);
+
+            return obj;
+        }
+
+        public void Despawn(GameObject obj)
+        {
+            if (obj.activeInHierarchy)
+            {
+                obj.SetActive(false);
+                obj.transform.parent = null;
+                inactiveObjects.Enqueue(obj);
+            }
+        }
+    }
+
+    private static void Init(GameObject prefab = null, int quantity = DEFAULT_POOL_SIZE)
+    {
+        if (prefab != null)
+        {
+            var prefabID = prefab.GetInstanceID();
+            if (!pools.ContainsKey(prefabID))
+                pools[prefabID] = new Pool(prefab, quantity);
+        }
+    }
+
+    public static void Preload(GameObject prefab, int quantity = 1)
+    {
+        Init(prefab, quantity);
+
+        for (int i = 0; i < quantity; i++)
+        {
+            Despawn(Spawn(prefab, Vector3.zero, Quaternion.identity));
+        }
+    }
+
+    public static GameObject Spawn(GameObject prefab, Vector3 pos, Quaternion rot, Transform parent = null)
+    {
+        Init(prefab);
+        return pools[prefab.GetInstanceID()].Spawn(pos, rot, parent);
+    }
+
+    public static T Spawn<T>(T component, Vector3 pos, Quaternion rot, Transform parent = null) where T : Component
+    {
+        return Spawn(component.gameObject, pos, rot, parent).GetComponent<T>();
+    }
+
+    public static GameObject Spawn(GameObject prefab, Vector3 pos, Transform parent = null)
+    {
+        Init(prefab);
+        return pools[prefab.GetInstanceID()].Spawn(pos, Quaternion.identity, parent);
+    }
+
+    public static T Spawn<T>(T component, Vector3 pos, Transform parent = null) where T : Component
+    {
+        return Spawn(component.gameObject, pos, parent).GetComponent<T>();
+    }
+
+    public static GameObject Spawn(GameObject prefab, Transform parent = null)
+    {
+        Init(prefab);
+        return pools[prefab.GetInstanceID()].Spawn(Vector3.zero, Quaternion.identity, parent);
+    }
+
+    public static T Spawn<T>(T component, Transform parent = null) where T : Component
+    {
+        return Spawn(component.gameObject, parent).GetComponent<T>();
+    }
+
+    public static void Despawn(GameObject go)
+    {
+        Pool assignedPool = null;
+        foreach (var pool in pools.Values)
+        {
+            if (pool.memberIDs.Contains(go.GetInstanceID()))
+            {
+                assignedPool = pool;
+                break;
+            }
+        }
+
+        if (assignedPool == null)
+        {
+            Object.Destroy(go);
+        }
+        else
+        {
+            assignedPool.Despawn(go);
+        }
+    }
+
+    public static void Despawn(GameObject go, float delay)
+    {
+        MEC.Timing.RunCoroutine(DespawnDelayed(go, delay));
+    }
+
+    private static IEnumerator<float> DespawnDelayed(GameObject go, float delay)
+    {
+        yield return MEC.Timing.WaitForSeconds(delay);
+        Despawn(go);
+    }
+}
+
+public static class SimplePoolGameObjectExtensions
+{
+    public static void Despawn(this GameObject go)
+    {
+        Pooling.Despawn(go);
+    }
+
+    public static void Despawn(this GameObject go, float delay)
+    {
+        Pooling.Despawn(go, delay);
+    }
+}
---
+++ Assets/_Project/SaveSystem/SaveManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    private static string SavePath => $"{Application.persistentDataPath}/save.sav";
+    private static Dictionary<string, object> data = new Dictionary<string, object>();
+    private static Coroutine saveCoroutine;
+    private static IFormatter formatter;
+
+    public static SaveManager Instance { get; private set; }
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Init()
+    {
+        Instance = new GameObject("SaveManager", typeof(SaveManager)).GetComponent<SaveManager>();
+        DontDestroyOnLoad(Instance.gameObject);
+        formatter = GetFormatter();
+    }
+
+    private void Awake() => data = LoadFile();
+
+    public static T Get<T>(string key, T def = default)
+    {
+        if (data.ContainsKey(key))
+            return (T)data[key];
+
+        return def;
+    }
+
+    public static void Set<T>(string key, T value)
+    {
+        data[key] = value;
+        Save();
+    }
+
+    private static void Save()
+    {
+        if (saveCoroutine != null)
+            Instance.StopCoroutine(saveCoroutine);
+
+        saveCoroutine = Instance.StartCoroutine(SaveCoroutine());
+    }
+
+    public static void DeleteAll()
+    {
+        data = null;
+        SaveFile(null);
+    }
+
+    private static IEnumerator SaveCoroutine()
+    {
+        yield return null;
+        SaveFile(data);
+    }
+
+    private static void SaveFile(object state)
+    {
+        using (var stream = File.Open(SavePath, FileMode.Create))
+        {
+            var formatter = GetFormatter();
+            formatter.Serialize(stream, state);
+        }
+    }
+
+    private static Dictionary<string, object> LoadFile()
+    {
+        if (!File.Exists(SavePath))
+        {
+            return new Dictionary<string, object>();
+        }
+
+        using (var stream = File.Open(SavePath, FileMode.Open))
+        {
+            var formatter = GetFormatter();
+            return (Dictionary<string, object>)formatter.Deserialize(stream);
+        }
+    }
+
+    private static IFormatter GetFormatter()
+    {
+        var formatter = new BinaryFormatter();
+
+        SurrogateSelector selector = new SurrogateSelector();
+        Vector3Surrogate vector3Surrogate = new Vector3Surrogate();
+        selector.AddSurrogate(typeof(Vector3), new StreamingContext(StreamingContextStates.All), vector3Surrogate);
+        formatter.SurrogateSelector = selector;
+
+        return formatter;
+    }
+}
